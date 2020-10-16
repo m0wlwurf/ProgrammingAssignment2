e8cf1cf863093e2443fdb242aee40e549f6c11fd
m0wlwurf <f_haering@web.de>
2020-10-16 14:47:52 +0200

    bigfix in makeCacheMatrix: Line 9: replaced m with i

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@ makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        i <<- NULL
     }
     get <- function() x
     setinv <- function(inv) i <<- inv
